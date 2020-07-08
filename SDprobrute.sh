293df16a68dda5db42b77e0af784c66c4e32cbd4
SDpro96 <sdpro09780@gmail.com>
2020-07-09 04:24:28 +0630

    by SDpro

---
+++ SDprobrute.sh
@@ -0,0 +1,90 @@
+#usr/bin/bash
+apt update -y
+apt upgrade -y
+pkg install python3
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+echo
+echo
+echo
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"     😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎 "$i"     |"
+echo $i" |"$me"─────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$ku" SDpro [SDpro96]"$i"                    |"
+echo $i" |"$pu" FACEBOOK :"$ku" Paye Pyoe Lin(SDpro)"$i"             |"
+echo $i" |"$pu" Phone :"$cy"09780393998                        "$i"  |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" 085835787069"$i"             |"
+echo $i" |"$pu" Contack Gmail :"$ku" kko636560@gmail.com"$i"         |"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 1"$i" |"$cy" SDprobrute               "$i"|"$i"   |"$cy" 🤑"$i"  |"
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me"99"$i" |"$cy" Exist                    "$i"|"$i"   |"$cy" Bye"$i" |"
+
+echo $i"────────────────────────────────"$i"    ─────"
+echo
+echo $me"┌==="$bi"["$i" SDpro "$bi"]"$me"======"$bi"["$i"Choice Number  "$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+if [ $pil = 1 ]
+then
+clear
+
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3
+cd Fb-Cracker-v.3
+clear
+echo '
+       __Hey guys :)
+   _  |@@|
+  / \ \--/ __
+  ) O|----|  |   __
+ / / \ }{ /\ )_ / _\
+ )/  /\__/\ \__O (__
+|/  (--/\--)    \__/
+/   _)(  )(_
+   ----------'| lolcat
+echo " "
+echo "\. ..Script By {SDpro}.../"| lolcat
+python2 crack.py
+
+fi
+if [ $pil = 99 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+echo '
+       __Hey guys :)
+   _  |@@|
+  / \ \--/ __
+  ) O|----|  |   __
+ / / \ }{ /\ )_ / _\
+ )/  /\__/\ \__O (__
+|/  (--/\--)    \__/
+/   _)(  )(_
+   ----------'| lolcat
+echo " "
+figlet -f slant " B Y E "|lolcat
+sleep 2
+echo $cy"Pls Follow me"
+sleep 2
+echo $i"Pls Subscribe "
+
+
+sleep 2
+echo $ku"Phone :"$i" 09780393998"
+echo $ku"Facebook :"$i" paye Pyoe Lin(SDpro)"
+sleep 3
+echo $pur">> Thanks YOU <<"
+echo $pur""
+exit
+fi
